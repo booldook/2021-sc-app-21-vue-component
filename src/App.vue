91c448057e544184afee60762bf5f4351e891c47
booldook <booldook@gmail.com>
2021-10-05 11:26:20 +0900

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,18 @@
 <template>
-	<div class="header-wrapper">
-		<h1 class="logo">Hello {{ name }}</h1>
-		<button @click="showAlert">눌러봐</button>
+	<div class="alert alert-primary">
+		<h1 class="text-center">
+			<i class="fa fa-bars"></i>
+			Hello World
+		</h1>
 	</div>
 </template>
 
 <script>
 export default {
-	name: 'App',
-	data() {
-		return {
-			name: 'Booldook'
-		}
-	},
-	methods: {
-		showAlert(e) {
-			alert('Hello '+this.name)
-		}
-	}
+
 }
 </script>
 
 <style lang="scss">
-.header-wrapper {
-	text-align: center;
-	h1.logo {
-		color: red;
-	}
-}
+
 </style>
