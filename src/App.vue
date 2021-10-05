59f1f94ace5fea9cecf43760441201f7e9b5c80b
booldook <booldook@gmail.com>
2021-10-05 12:31:01 +0900

    ImgCp 작업중

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <template>
-	<div class="alert alert-primary">
-		<h1 class="text-center">
-			<i class="fa fa-bars"></i>
-			Hello World
-		</h1>
+	<div class="app-wrapper">
+		<ImgCp />
 	</div>
 </template>
 
 <script>
-export default {
+import axios from 'axios'
+import ImgCp from './components/ImgCp.vue'
 
+export default {
+	name: 'App',
+	components: { ImgCp },
+	beforeCreate() {
+		console.log('beforeCreate')
+	},
+	beforeMount() {
+		console.log('beforeMount')
+	},
+	async created() {
+		console.log('Create')
+		const { data } = await axios.get('/json/foods.json')
+		console.log(data)
+	}
 }
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.app-wrapper {
+	max-width: 400px;
+	margin: 2em auto;
+}
 </style>
