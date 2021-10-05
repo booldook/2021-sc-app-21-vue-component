0f7f8fa608dfc4ece6cbe8f7bf937594fc1e8e0e
booldook <booldook@gmail.com>
2021-10-05 13:15:58 +0900

    thumb 불러오기 완료

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,54 @@
 <template>
 	<div class="app-wrapper">
-		<ImgCp />
+		<ImgCp :title="title" :src="src" />
+		<ul class="thumb-wrap">
+			<ThumbCp v-for="v in foods" :key="v.id" :img="v" />
+		</ul>
 	</div>
 </template>
 
 <script>
+/* 
+1. 부모 컴포넌트가 자식 컴포넌트에게 변수를 전달할 때는 v-bind로 전달한다.
+2. 자식 컴포넌트는 부모로 부터 props로 변수를 전달받는다.
+*/
+
 import axios from 'axios'
 import ImgCp from './components/ImgCp.vue'
+import ThumbCp from './components/ThumbCp.vue'
 
 export default {
 	name: 'App',
-	components: { ImgCp },
-	beforeCreate() {
-		console.log('beforeCreate')
-	},
-	beforeMount() {
-		console.log('beforeMount')
+	components: { ImgCp, ThumbCp },
+	data() {
+		return {
+			foods: [],
+			title: '',
+			src: '',
+		}
 	},
 	async created() {
-		console.log('Create')
 		const { data } = await axios.get('/json/foods.json')
-		console.log(data)
+		this.foods = data
+		this.title = this.foods[0].title
+		this.src = this.foods[0].src
 	}
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+ul, ol {
+	list-style: none;
+}
 .app-wrapper {
 	max-width: 400px;
-	margin: 2em auto;
+	margin: 2em auto 1em auto;
+	.thumb-wrap {
+		border: 1px solid #efefef;
+		border-radius: .325em;
+		padding: .5em;
+		display: flex;
+		width: 101%;
+	}
 }
 </style>
--- src/components/ImgCp.vue
+++ src/components/ImgCp.vue
@@ -1,13 +1,14 @@
 <template>
 	<div class="img-wrapper">
-		<img src="/img/f1.jpg" alt="아이스크림" class="w-100">
-		<div class="title-wrap">아이스크림</div>
+		<img :src="src" alt="아이스크림" class="w-100">
+		<div class="title-wrap">{{ title }}</div>
 	</div>
 </template>
 
 <script>
 export default {
-	name: 'ImgCp'
+	name: 'ImgCp',
+	props: ['title', 'src']
 }
 </script>
 
@@ -17,6 +18,7 @@ export default {
 	padding: .5em;
 	border-radius: .325em;
 	position: relative;
+	margin-bottom: 1em;
 	img {
 		opacity: 1;
 		transition: all .5s;
